b9ca55dbe1850c846442813c3082c8ccb65ed244
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2021-05-29 07:17:33 -0700

    feat: Restart

--- src/util.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-
-namespace libvpk
-{
-	std::string ExtractDirectory(const std::string& fullpath);
-	std::string ExtractFileExtension(const std::string& fullpath);
-}
